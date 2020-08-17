c1fed0ef2d92d6a0e20edcefcea90530bee87e21
Sridhar <sridhar.pg@zohocorp.com>
2020-08-17 13:41:18 +0530

    Add toggle and passing values to yield

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import {action, set} from '@ember/object';
+import {action, get, set} from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 class Form {
@@ -62,15 +62,57 @@ export default class ApplicationController extends Controller {
         value: ''
       }
     ]
+  }, {
+    id: 3,
+    name: 'Group 3',
+    fields: [
+      {
+        name: 'First Name',
+        value: ''
+      }, {
+        name: 'Last Name',
+        value: ''
+      }, {
+        name: 'Mobile',
+        value: ''
+      }
+    ]
   }]
+
+  @tracked isIamAttending = false;
+
+  copyValuesFromOrder(index) {
+    set(this.groups[index].fields[0], 'value', this.orderForm.fields[0].value);
+    set(this.groups[index].fields[1], 'value', this.orderForm.fields[1].value);
+    set(this.groups[index].fields[2], 'value', this.orderForm.fields[2].value);
+  }
+
+  setProps(key, value) {
+    set(this.groups[0].fields[0], key, value);
+    set(this.groups[0].fields[1], key, value);
+    set(this.groups[0].fields[2], key, value);
+  }
 
   @action
   changed(event) {
-    set(this.groups[0].fields[0], 'disabled', event.target.checked);
-    set(this.groups[0].fields[1], 'disabled', event.target.checked);
-    if(event.target.checked) {
-      set(this.groups[0].fields[0], 'value', this.orderForm.fields[0].value);
-      set(this.groups[0].fields[1], 'value', this.orderForm.fields[1].value);
+    this.isIamAttending = event.target.checked;
+
+    this.setProps('disabled', this.isIamAttending);
+
+    if(this.isIamAttending) {
+      this.copyValuesFromOrder(0);
     }
+  }
+
+  @action
+  copyFromOrder(index) {
+    this.copyValuesFromOrder(index);
+  }
+
+  @action
+  toggleCommonOptions() {
+    let isHidden = !get(this.groups[0], '_showCommonOptions');
+    set(this.groups[0], '_showCommonOptions', isHidden);
+    this.setProps('_hidden', isHidden);
   }
 }
